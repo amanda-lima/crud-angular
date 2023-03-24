9078e9f84b47cd193f3f69bc8b326132668b7265
Amanda Lima <amanda.limaa0202@gmail.com>
2023-03-24 16:41:00 -0300

    feat: Utilizando implementações em app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { ListComponent } from './components/list/list.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { CPFPipe } from './utils/cpfPipe/cpf.pipe';
+import { PhonePipe } from './utils/phonePipe/phone.pipe';
+import { InputMaskModule } from '@ngneat/input-mask';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ListComponent,
+    ModalComponent,
+    CPFPipe,
+    PhonePipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    InputMaskModule
+ ],
   providers: [],
   bootstrap: [AppComponent]
 })
